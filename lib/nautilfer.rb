70ae0dbbbebb8a9c17416eee1af7075fa58011a9
Yusuke <yusuke@slidict.io>
2024-09-02 19:22:18 +0900

    first commit

---
+++ lib/nautilfer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "nautilfer/version"
+
+module Nautilfer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/nautilfer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Nautilfer do
+  it "has a version number" do
+    expect(Nautilfer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
